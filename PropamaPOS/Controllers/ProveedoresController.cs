30e2567ebf06d0b5f9ce3fbd20ca885d5366092d
José Maltez <jmaltezv@miumg.edu.gt>
2025-08-28 22:26:39 -0600

    se creo el controlador proveedores

---
+++ PropamaPOS/Controllers/ProveedoresController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PropamaPOS.Services;
+using System.Threading.Tasks;
+
+namespace PropamaPOS.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        private readonly ProveedorServiceClient _service;
+
+        public ProveedoresController(ProveedorServiceClient service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var proveedores = await _service.GetProveedoresAsync();
+            return View(proveedores);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProveedorDto proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.CreateProveedorAsync(proveedor);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proveedor);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var proveedor = await _service.GetProveedorAsync(id);
+            return View(proveedor);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, ProveedorDto proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.UpdateProveedorAsync(id, proveedor);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(proveedor);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var proveedor = await _service.GetProveedorAsync(id);
+            return View(proveedor);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _service.DeleteProveedorAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
