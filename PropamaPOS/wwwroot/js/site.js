4a31ddab692ab23517d77e801d82ceaa35fe3f49
José Maltez <jmaltezv@miumg.edu.gt>
2025-09-09 16:54:04 -0600

    se agrego un spinner al ingresar en el boton login

--- PropamaPOS/wwwroot/js/site.js
+++ PropamaPOS/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// spinner login
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('loginForm');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', function () {
+            const btnLogin = document.getElementById('btnLogin');
+            const loginText = document.getElementById('loginText');
+            const loginSpinner = document.getElementById('loginSpinner');
+
+            loginText.classList.add('d-none');
+            loginSpinner.classList.remove('d-none');
+            btnLogin.disabled = true;
+        });
+    }
+});
