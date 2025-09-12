66b3f257b61170f1dbc9995d0f25e71e8a2b84bf
José Maltez <jmaltezv@miumg.edu.gt>
2025-09-11 22:27:37 -0600

    se crearon toggles en las vistas para ver las contraseñas si se desea, se cambio el logo de un modulo

--- PropamaPOS/wwwroot/js/site.js
+++ PropamaPOS/wwwroot/js/site.js
@@ -30,3 +30,21 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".toggle-password").forEach(btn => {
+        btn.addEventListener("click", function () {
+            const input = this.previousElementSibling; // el input está antes del botón
+            const icon = this.querySelector("i");
+            if (input.type === "password") {
+                input.type = "text";
+                icon.classList.remove("bi-eye");
+                icon.classList.add("bi-eye-slash");
+            } else {
+                input.type = "password";
+                icon.classList.remove("bi-eye-slash");
+                icon.classList.add("bi-eye");
+            }
+        });
+    });
+});
